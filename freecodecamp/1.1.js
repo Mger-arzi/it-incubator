d24b00f2286d27634f4616317db5fb246fd31248
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-23 18:49:22 +0300

    freecodecamp

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -59,3 +59,18 @@ function reusableFunction(){
 	console.log(a+b)
   }
   functionWithArgs(2,3)
+
+  let myGlobal =10
+function fun1() {
+oopsGlobal = 5
+}
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
