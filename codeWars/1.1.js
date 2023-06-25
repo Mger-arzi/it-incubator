9f6957cb50d66f6c2551545a7acbbc64ecc4e75a
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-25 10:56:12 +0300

    codeWars

--- codeWars/1.1.js
+++ codeWars/1.1.js
@@ -29,3 +29,54 @@ function findShort(s){
 	let arr = s.split(" ").sort((a, b) => a.length - b.length);
 	return arr[0].length
   }
+
+  function isTriangle(a, b, c) {
+	if ((a + b > c) && (a + c > b) && (b + c > a)) {
+	return true;
+	}
+	else {
+	if ((a <= 0) || (b <= 0) || (c <= 0)) {
+	return false;
+	}
+	else {
+	return false;
+	}
+	}
+	}
+
+	let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+function equal2(){
+  let a = v3 ,
+      b = v1; 
+  return a - b;
+}
+
+function equal3(){
+  let a = v1 , 
+      b = v5 ; 
+  return a * b;
+}
+
+function equal4(){
+  let a = v4 , 
+      b = v5 ; 
+  return a / b;
+}
+
+function equal5(){
+  let a = v2 , 
+      b =  v4; 
+  return a % b;
+}
