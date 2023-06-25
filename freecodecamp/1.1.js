9e973f96798da3540d3330a46e1863ff38511f36
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-25 11:12:25 +0300

    freecodecamp

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -178,3 +178,18 @@ function testLogicalAnd(val) {
 	return "Inside";
   }
 testLogicalOr(15);
+
+function testElse(val) {
+	let result = "";
+  
+	if (val > 5) {
+	  result = "Bigger than 5";
+	}
+  
+	else  {
+	  result = "5 or Smaller";
+	}
+	return result;
+  }
+  
+  testElse(4);
