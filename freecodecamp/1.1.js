aa547c09acbdf50503589dec9c1b869261324da0
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-26 18:01:16 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -298,3 +298,27 @@ function caseInSwitch(val) {
     break
   }
 }
+
+function switchOfStuff(val) {
+  let answer = "";
+  switch(val){
+    case "a" :
+    answer = "apple"
+    break;
+
+    case 'b' :
+    answer = "bird"
+    break;
+
+    case 'c' :
+    answer = "cat"
+    break;
+
+    default:
+    answer = "stuff"
+    break;
+  }
+
+
+  return answer;
+}
