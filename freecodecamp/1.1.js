4312f5cecb14d563f233ae1c463ad01b8fdb6939
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-26 17:52:36 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -277,3 +277,24 @@ if (strokes == 1 ){
 }
 
 golfScore(5, 4);
+
+function caseInSwitch(val) {
+  let answer = "";
+  switch(val){
+    case 1 :
+    answer = "alpha"
+    break
+
+    case 2 :
+    answer = "beta"
+    break
+
+    case 3 :
+    answer = "gamma"
+    break
+
+    case 4 :
+    answer = "delta"
+    break
+  }
+}
