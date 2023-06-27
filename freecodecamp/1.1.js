a36b4835ed86af84fb2c5d0d8e485c467e018d79
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-27 19:48:18 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -386,3 +386,12 @@ function isLess(a, b) {
 }
 
 isLess(10, 15);
+
+function abTest(a, b) {
+if (a < 0 || b < 0){
+  return undefined
+}
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
