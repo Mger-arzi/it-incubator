b999f5d41fd49c26c8acefc7ee5343a64f72be40
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-29 12:47:01 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -483,3 +483,27 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": 'nklj',
+    "title": 'hff',
+    "release_year": 1998,
+    "formats": [
+      'cs',
+      'sd',
+      're'
+    ]
+  }
+];
