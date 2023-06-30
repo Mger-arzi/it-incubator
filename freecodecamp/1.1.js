029251474d1f5f16d038fbafaf1d5a94a586f9c1
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-06-30 15:06:27 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -594,3 +594,14 @@ const myArray = [];
 for ( let i = 9; i > 0; i -= 2){
   myArray.push(i)
 }
+function multiplyAll(arr) {
+	let product = 1;
+  for (let i = 0; i < arr.length; i++){
+	for (let j = 0; j < arr[i].length; j++){
+	  product *= arr[i][j]
+	}
+  }
+	return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
