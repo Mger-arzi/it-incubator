0f182732e10f04575cf0a3f615b578f1a050beaf
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-07-07 14:24:26 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -684,3 +684,8 @@ function convertToInteger(str) {
 	return MATH_CONSTANTS.PI;
   }
   const PI = freezeObj();
+
+  const myConcat =(arr1, arr2) =>  
+  arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
