2a04053cbc089de45d8dcd028ad887fb4925588a
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-07-07 14:15:10 +0300

    Add files via upload

--- freecodecamp/1.1.js
+++ freecodecamp/1.1.js
@@ -671,3 +671,16 @@ function convertToInteger(str) {
 	? []
 	: rangeOfNumbers(startNum ,endNum - 1).concat(endNum)  
    };
+   function freezeObj() {
+	const MATH_CONSTANTS = {
+	  PI: 3.14
+	};
+  Object.freeze(MATH_CONSTANTS)
+	try {
+	  MATH_CONSTANTS.PI = 99;
+	} catch(ex) {
+	  console.log(ex);
+	}
+	return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
