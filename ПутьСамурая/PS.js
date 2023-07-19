6326ebb10748bd798c078447750bb457838d36d8
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-07-19 09:56:10 +0300

    Add files via upload

--- ПутьСамурая/PS.js
+++ ПутьСамурая/PS.js
@@ -1,3 +1,17 @@
 function randomRange(myMin, myMax) {
-	return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+let p = new Promise(function (resolve, reject) {
+  let isGoodVideo = false;
+  if (isGoodVideo) {
+    resolve("Is video nise");
+  } else {
+    reject("Is video failed");
   }
+});
+p.then((mess) => {
+  console.log("Mess:" + mess);
+}).catch((error) => {
+  console.warn("Error:" + error);
+});
