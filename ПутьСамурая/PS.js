35f1cdcd83de3fa1db4fa55c1053898386f042f3
Mger <118669151+Mger-arzi@users.noreply.github.com>
2023-07-19 10:28:19 +0300

    Add files via upload

--- ПутьСамурая/PS.js
+++ ПутьСамурая/PS.js
@@ -15,3 +15,43 @@ p.then((mess) => {
 }).catch((error) => {
   console.warn("Error:" + error);
 });
+
+let MgerLoveNat = true;
+let NatLoveMger = false;
+
+// function LifeMgerAndNat(life, error) {
+//   if (MgerLoveNat && NatLoveMger) {
+//     life({
+//       life: "happiness",
+//       name: "HZ",
+//     });
+//   } else error("Hyi vam");
+// }
+
+// LifeMgerAndNat(
+//   (mess) => {
+//     console.log("life " + mess.life);
+//   },
+//   (error) => {
+//     console.warn("Err " + error);
+//   }
+// );
+
+let life = new Promise(function (resolve, reject) {
+  let MgerLoveNat = true;
+  let NatLoveMger = false;
+
+  if (MgerLoveNat && NatLoveMger) {
+    resolve("happiness");
+  } else {
+    reject("H 	U 	I");
+  }
+});
+
+life
+  .then((mess) => {
+    console.log("Happi: " + mess);
+  })
+  .catch((error) => {
+    console.warn("Err: " + error);
+  });
